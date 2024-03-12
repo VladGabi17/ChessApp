28d10dc32672cb8a45c1fac5f418a8dad214826a
VladGabi17 <163211947+VladGabi17@users.noreply.github.com>
2024-03-13 01:14:52 +0200

    Add files via upload

---
+++ chess.js
@@ -0,0 +1,8 @@
+function changeColor(color) {
+    const zone = document.querySelector('.zone');
+    zone.style.backgroundColor = color;
+}
+
+function changeImg(anything){
+    document.querySelector('.chessimg').src = anything;
+}
